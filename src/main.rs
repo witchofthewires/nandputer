a920a4bf5fa727eceedcb7140ee38390a24d517e
dani <dcm678@gmail.com>
2024-11-23 16:00:15 -0500

    added unit tests for all existing gates

--- src/main.rs
+++ src/main.rs
@@ -64,3 +64,54 @@ fn nor(val1: bool, val2: bool) -> bool {
 fn xor(val1: bool, val2: bool) -> bool {
     and(or(val1, val2), nand(val1, val2))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_nand_works() {
+        assert_eq!(nand(false, false), true);
+        assert_eq!(nand(false, true), true);
+        assert_eq!(nand(true, false), true);
+        assert_eq!(nand(true, true), false);
+    }
+    
+    #[test]
+    fn test_not_works() {
+        assert_eq!(not(false), true);
+        assert_eq!(not(true), false);
+    }
+
+    #[test]
+    fn test_and_works() {
+        assert_eq!(and(false, false), false);
+        assert_eq!(and(false, true), false);
+        assert_eq!(and(true, false), false);
+        assert_eq!(and(true, true), true);
+    }
+
+    #[test]
+    fn test_or_works() {
+        assert_eq!(or(false, false), false);
+        assert_eq!(or(false, true), true);
+        assert_eq!(or(true, false), true);
+        assert_eq!(or(true, true), true);
+    }
+
+    #[test]
+    fn test_nor_works() {
+        assert_eq!(nor(false, false), true);
+        assert_eq!(nor(false, true), false);
+        assert_eq!(nor(true, false), false);
+        assert_eq!(nor(true, true), false);
+    }
+
+    #[test]
+    fn test_xor_works() {
+        assert_eq!(xor(false, false), false);
+        assert_eq!(xor(false, true), true);
+        assert_eq!(xor(true, false), true);
+        assert_eq!(xor(true, true), false);
+    }
+}
