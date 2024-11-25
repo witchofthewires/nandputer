e9efb8d38d4d0911add603c077f7e8821426f49a
dani <dcm678@gmail.com>
2024-11-25 09:43:10 -0500

    implemented bytes_to_boolvec, multibit muxes

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,12 @@ fn or16(val1: &Vec<bool>, val2: &Vec<bool>) -> Vec<bool> {
     }
     res
 }
-
+/* 
+fn or8way(val: &Vec<bool>) -> bool {
+    println!("{val} TODO IMPLEMENT");
+    false
+}
+*/
 fn nor(val1: bool, val2: bool) -> bool {
     not(or(val1, val2))
 }
@@ -102,9 +107,41 @@ fn mux16(val1: &Vec<bool>, val2: &Vec<bool>, sel: bool) -> Vec<bool> {
     res
 }
 
+fn mux4way16(val1: &Vec<bool>, val2: &Vec<bool>, val3: &Vec<bool>, val4: &Vec<bool>, sel: (bool, bool)) -> Vec<bool> { 
+    mux16(&mux16(val1, val3, sel.0), 
+          &mux16(val2, val4, sel.0), 
+          sel.1)
+}
+
+fn mux8way16(val1: &Vec<bool>, 
+             val2: &Vec<bool>, 
+             val3: &Vec<bool>, 
+             val4: &Vec<bool>, 
+             val5: &Vec<bool>, 
+             val6: &Vec<bool>, 
+             val7: &Vec<bool>, 
+             val8: &Vec<bool>, 
+             sel: (bool, bool, bool)) -> Vec<bool> { 
+    mux16(&mux4way16(val1, val2, val3, val4, (sel.1, sel.2)), 
+          &mux4way16(val5, val6, val7, val8, (sel.1, sel.2)), 
+          sel.0)
+}
+
 fn dmux(val: bool, sel: bool) -> (bool, bool) {
     (and(val, not(sel)), 
      and(val, sel))
+}
+
+fn bytes_to_boolvec(bytes: &[u8]) -> Vec<bool> {
+    let mut boolvec = Vec::new();
+    for byte in bytes {
+        for i in 0..8 {
+            let val = (byte >> (7-i)) & 1;
+            if val == 1 { boolvec.push(true); }
+            else { boolvec.push(false); }
+        }
+    }
+    boolvec
 }
 
 #[cfg(test)]
@@ -206,5 +243,54 @@ mod tests {
         let val2 = vec![false, false, true, true, false, true, true, true, true, false, false, true, true, true, true, false];
         assert_eq!(mux16(&val1, &val2, false), val1);
         assert_eq!(mux16(&val1, &val2, true), val2);
+    }
+
+    #[test]
+    fn test_mux4way16_works() {
+        let val1 = bytes_to_boolvec(&[0x5d, 0x1b]);
+        let val2 = bytes_to_boolvec(&[0x37, 0x9e]);
+        let val3 = bytes_to_boolvec(&[0x9f, 0x66]);
+        let val4 = bytes_to_boolvec(&[0x54, 0xd3]);
+        assert_eq!(mux4way16(&val1, &val2, &val3, &val4, (false, false)), val1);
+        assert_eq!(mux4way16(&val1, &val2, &val3, &val4, (false, true)), val2);
+        assert_eq!(mux4way16(&val1, &val2, &val3, &val4, (true, false)), val3);
+        assert_eq!(mux4way16(&val1, &val2, &val3, &val4, (true, true)), val4);
+    }
+
+    #[test]
+    fn test_mux8way16_works() {
+        let val1 = bytes_to_boolvec(&[0x5d, 0x1b]);
+        let val2 = bytes_to_boolvec(&[0x37, 0x9e]);
+        let val3 = bytes_to_boolvec(&[0x9f, 0x66]);
+        let val4 = bytes_to_boolvec(&[0x54, 0xd3]);
+        let val5 = bytes_to_boolvec(&[0x12, 0x34]);
+        let val6 = bytes_to_boolvec(&[0x56, 0x78]);
+        let val7 = bytes_to_boolvec(&[0x90, 0xab]);
+        let val8 = bytes_to_boolvec(&[0xcd, 0xef]);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (false, false, false)), val1);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (false, false, true)), val2);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (false, true, false)), val3);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (false, true, true)), val4);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (true, false, false)), val5);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (true, false, true)), val6);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (true, true, false)), val7);
+        assert_eq!(mux8way16(&val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, (true, true, true)), val8);
+    }
+/* 
+    #[test]
+    fn test_or8way_works() {
+        for i in 0..=255 {
+            let bytes: u8 = [i];
+            let mut expected: bool = true;
+            if bytes[0] == 0: expected = false;
+            assert_eq!(or8way(bytes_to_boolvec(&bytes)), expected);
+        }
+    }
+*/
+    #[test]
+    fn test_bytes_to_boolvec_works() {
+        let val = vec![false, true, false, true, true, true, false, true, false, false, false, true, true, false, true, true];
+        let val_bytes = [0x5d, 0x1b];
+        assert_eq!(bytes_to_boolvec(&val_bytes), val);
     }
 }
