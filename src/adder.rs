9dd87cfdff329ba502697d84172373f632ed4c33
dani <dcm678@gmail.com>
2024-11-28 13:18:55 -0500

    added doc comments for all functions

--- src/adder.rs
+++ src/adder.rs
@@ -2,11 +2,19 @@ use std::iter;
 
 use crate::gates;
 
+/// half_adder - Add two bits
+/// Inputs: bit1, bit2
+/// Outputs: (sum, carry)
+/// Function: sum is LSB of bit1 + bit2, carry is MSB
 fn half_adder(bit1: bool, bit2: bool) -> (bool, bool) {
     (gates::xor(bit1, bit2), 
      gates::and(bit1, bit2))
 }
 
+/// half_adder - Add two bits and carry input
+/// Inputs: bit1, bit2, carry
+/// Outputs: (sum, carry_out)
+/// Function: sum is LSB of bit1 + bit2 + carry, carry_out is MSB
 fn full_adder(bit1: bool, bit2: bool, carry: bool) -> (bool, bool) {
     let (half_sum, half_carry_1) = half_adder(bit1, bit2);
     let (full_sum, half_carry_2) = half_adder(half_sum, carry);
@@ -15,6 +23,13 @@ fn full_adder(bit1: bool, bit2: bool, carry: bool) -> (bool, bool) {
 
 // carry-ripple adder
 // TODO replace with carry-lookahead
+/// Add16 - Add two 16-bit values
+/// Inputs: val1[16], val2[16]
+/// Outputs: res[16]
+/// Function: res=val1+val2
+/// 
+/// Integer 2’s complement addition.
+/// Overflow is neither detected nor handled.
 fn add16(val1: &Vec<bool>, val2: &Vec<bool>) -> Vec<bool> {
     let mut res: [bool; 16] = [false; 16];
     let mut carry_bus: [bool; 17] = [false; 17]; // prevent bus overflow on final bit
@@ -31,6 +46,13 @@ fn add16(val1: &Vec<bool>, val2: &Vec<bool>) -> Vec<bool> {
     Vec::from(res)
 }
 
+/// Inc16 - Increment value by 1
+/// Inputs: val[16]
+/// Outputs: res[16]
+/// Function: res=val+1 
+/// 
+/// Integer 2’s complement addition.
+/// Overflow is neither detected nor handled.
 fn inc16(val: &Vec<bool>) -> Vec<bool> {
     let one = gates::bytes_to_boolvec(&[1]);
     add16(&val, &one)
